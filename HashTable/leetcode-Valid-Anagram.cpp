51de085c8a17f51131c08107d9fc81ace63037c7
Ellery Queen <yhmsc007@hotmail.com>
2016-03-11 14:41:52 +0800

    using sort is easy

---
+++ HashTable/leetcode-Valid-Anagram.cpp
@@ -0,0 +1,11 @@
+//leetcode
+//Single Number II
+//Easy
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s == t;
+    }
+};
