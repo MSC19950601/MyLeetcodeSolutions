628e3de5ad174551cf1dac4a52825f1f6417822d
Ellery Queen <yhmsc007@hotmail.com>
2016-03-13 23:24:19 +0800

    an old solution

---
+++ Array/leetcode-Remove-Element.cpp
@@ -0,0 +1,9 @@
+//leecode
+//Remove Element
+//easy
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        return std::distance(nums.begin(),std::remove(nums.begin(),nums.end(),val));
+    }
+};
