611584ffccb2c64617f835fc8310591cd1d36ee5
Ellery Queen <yhmsc007@hotmail.com>
2016-03-25 21:53:57 +0800

    想起当年队长给我做的题~

---
+++ LinkedList/leetcode-Linked-List-Cycle.cpp
@@ -0,0 +1,28 @@
+//leetcode
+//leetcode-Linked-List-Cycle
+//Medium
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head ==NULL)   return false;
+        ListNode *p = head;
+        ListNode *q = head;
+        while(q->next!=NULL && q->next->next != NULL){
+            p = p->next;
+            q = q->next->next;
+            if(p == q){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
