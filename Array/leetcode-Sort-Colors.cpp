2a2008776d370d3423971dc1ef46fbe496f808d1
Ellery Queen <yhmsc007@hotmail.com>
2016-03-27 22:15:22 +0800

    one line Easy problem~

---
+++ Array/leetcode-Sort-Colors.cpp
@@ -0,0 +1,31 @@
+//leetcode
+//Sort Colors
+//Medium
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    }
+};
+//However,I don't know why others use so much code
+class Solution {
+public:
+//use counting sort
+void sortColors(int A[], int n) {
+int red = -1, white = -1, blue = -1;
+
+for(int i = 0; i < n; i++){
+    if(A[i] == 0){   
+        A[++blue] = 2;
+        A[++white] = 1;
+        A[++red] = 0;
+    }
+    else if(A[i] == 1){
+        A[++blue] = 2;
+        A[++white] = 1;
+    }
+    else if(A[i] == 2)   
+        A[++blue] = 2;
+}
+}
+}
