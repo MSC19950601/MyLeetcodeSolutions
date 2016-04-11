164bcb3f29e10a60b5ebd31d3423b26370d077cc
Ellery Queen <429889664@qq.com>
2016-04-11 14:37:45 +0800

    值得反思的一道贪心算法的题

---
+++ Greedy/Patching-Array.cpp
@@ -0,0 +1,19 @@
+//leetcode
+//Patching Array
+//Medium
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long miss=1,i=0,added=0;
+        while(miss<=n){
+            if(i < nums.size() && nums[i] <= miss){
+                miss += nums[i++];
+            }else{
+                miss += miss;
+                added++;
+            }
+        }
+        return added;
+        
+    }
+};
